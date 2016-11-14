ef8094d34c0e2930a8de973e6d35097cff9191ba
Jessica Tracy <j.tracy916@gmail.com>
2016-11-13 20:38:16 -0500

    Angular and end-to-end register with h2 db

---
+++ src/main/resources/static/js/ng-controller.js
@@ -0,0 +1,25 @@
+angular.module('StudyApp', [])
+   .controller('SampleController', function($scope, $http) {
+
+        $scope.register = function(firstName, lastName, registerEmail, registerPassword) {
+                    console.log("In register function in ng controller");
+
+                    var newUserInfo = {
+                        firstName: firstName,
+                        lastName: lastName,
+                        email: registerEmail,
+                        password: registerPassword
+                    }
+
+                    $http.post("/register.json", newUserInfo)
+                        .then(
+                            function successCallback(response) {
+                                console.log(response.data);
+                            },
+                            function errorCallback(response) {
+                                console.log("Unable to get data...");
+                            });
+                };
+
+
+   });
